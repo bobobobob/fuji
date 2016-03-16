7318f874b891e806d9bfc729dbea357980bdb85f
OCAO Kazutaka Ogaki <ocaokgbu@gmail.com>
2016-03-16 13:53:22 +0900

    fix message topic selection on serial device

--- device/serial.go
+++ device/serial.go
@@ -238,7 +238,7 @@ func (device SerialDevice) Start(channel chan message.Message) error {
 				channel <- msg
 			case msg, _ := <-device.DeviceChan.Chan:
 				log.Infof("msg topic:, %v / %v", msg.Topic, device.Name)
-				if !strings.HasSuffix(msg.Topic, device.Name) {
+				if !strings.HasSuffix(msg.Topic, strings.Join([]string{device.Name, device.Type, "subscribe"}, "/")) {
 					continue
 				}
 				log.Infof("msg reached to device, %v", msg)
