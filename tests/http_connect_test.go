094e1a78738648b46e2cce04bfbd3aad06db3ddd
OCAO Kazutaka Ogaki <ocaokgbu@gmail.com>
2016-09-06 15:39:30 +0900

    step by step shutdown

--- tests/http_connect_test.go
+++ tests/http_connect_test.go
@@ -454,9 +454,9 @@ func generalTestProcess(t *testing.T, httpConfigStr string, expected []string, h
 		assert.Equal("subscribe completed in 11 sec", "not completed")
 	}
 
+	time.Sleep(100 * time.Millisecond)
 	httpCommandChannel <- "done"
+	time.Sleep(100 * time.Millisecond)
 	fujiCommandChannel <- "close"
-	time.Sleep(300 * time.Millisecond)
-	client.Disconnect(250)
 	time.Sleep(300 * time.Millisecond)
 }
